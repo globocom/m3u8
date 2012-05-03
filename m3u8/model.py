278e7abe1f9f776893f2d74205b2d419fbd9afb9
Igor Sobreira <igor@igorsobreira.com>
2012-05-03 15:12:38 -0300

    model: add basepath attribute to update the urls of a playlist, refs #5

--- m3u8/model.py
+++ m3u8/model.py
@@ -8,13 +8,62 @@ class M3U8(object):
     '''
     Represents a single M3U8 playlist. Should be instantiated with
     the content as string.
+
+    Parameters:
+
+     `content`
+       the m3u8 content as string
+
+     `basepath`
+       all urls (key and segments url) will be updated with this basepath,
+       ex.:
+           basepath = "http://videoserver.com/hls"
+
+            /foo/bar/key.bin           -->  http://videoserver.com/hls/key.bin
+            http://vid.com/segment1.ts -->  http://videoserver.com/hls/segment1.ts
+
+       can be passed as parameter or setted as an attribute to ``M3U8`` object.
+
+    Attributes:
+
+     `key`
+       it's a `Key` object, the EXT-X-KEY from m3u8. Or None
+
+     `segments`
+       a `SegmentList` object, represents the list of `Segment`s from this playlist
+
+
+     .. TODO: document other attributes ..
+
     '''
 
-    def __init__(self, content):
+    def __init__(self, content, basepath=None):
         self.data = parser.parse(content)
+        self._initialize_attributes()
+        self.basepath = basepath
+
+    def _initialize_attributes(self):
+        self.key = Key(**self.data['key']) if 'key' in self.data else None
+        self.segments = SegmentList([ Segment(**params) for params in self.data['segments'] ])
 
     def __unicode__(self):
         return self.dumps()
+
+    @property
+    def basepath(self):
+        return self._basepath
+
+    @basepath.setter
+    def basepath(self, newbasepath):
+        self._basepath = newbasepath
+        self._update_basepath()
+
+    def _update_basepath(self):
+        if self._basepath is None:
+            return
+        if self.key:
+            self.key.basepath = self.basepath
+        self.segments.basepath = self.basepath
 
     def dumps(self):
         '''
@@ -29,16 +78,11 @@ class M3U8(object):
         if self.version:
             output.append('#EXT-X-VERSION:' + self.version)
         if self.key:
-            output.append(self.key_as_string)
+            output.append(str(self.key))
         if self.target_duration:
             output.append('#EXT-X-TARGETDURATION:' + str(self.target_duration))
 
-        for segment in self.segments:
-            extinf = '#EXTINF:%s,' % segment.duration
-            if segment.title:
-                extinf += '"%s"' % segment.title
-            output.append(''.join(extinf))
-            output.append(segment.uri)
+        output.append(str(self.segments))
 
         return '\n'.join(output)
 
@@ -101,76 +145,12 @@ class M3U8(object):
         return self.data.get('allow_cache')
 
     @property
-    def key(self):
-        '''
-        Returns a namedtuple 'Key' used to encrypt the segments (EXT-X-KEY)
-
-        `method`
-          is a string. ex.: "AES-128"
-
-        `uri`
-          is a string. ex:: "https://priv.example.com/key.php?r=52"
-
-        `iv`
-          initialization vector. a string representing a hexadecimal number. ex.: 0X12A
-
-        '''
-        if 'key' not in self.data:
-            return None
-
-        Key = namedtuple('Key', ['method', 'uri', 'iv'])
-        return Key(method=self.data['key']['method'],
-                   uri=self.data['key']['uri'],
-                   iv=self.data['key'].get('iv'))
-
-    @property
-    def key_as_string(self):
-        if not self.key:
-            return ''
-
-        output = [
-            'METHOD=%s' % self.key.method,
-            'URI="%s"' % self.key.uri,
-            ]
-
-        if self.key.iv:
-            output.append('IV=%s' % self.key.iv)
-
-        return '#EXT-X-KEY:' + ','.join(output)
-
-    @property
-    def segments(self):
-        '''
-        Returns an iterable with all .ts segments from playlist, in order.
-        Each segment is a namedtuple `Segment` with the attributes
-
-        `uri`
-          a string with the segment uri
-
-        `title`
-          title attribute from EXTINF parameter
-
-        `duration`
-          duration attribute from EXTINF paramter
-
-        '''
-        Segment = namedtuple('Segment', ['uri', 'title', 'duration'])
-
-        segments = []
-        for segment in self.data['segments']:
-            segments.append(Segment(uri=segment['uri'],
-                                    title=segment.get('title'),
-                                    duration=segment.get('duration')))
-
-        return segments
-
-    @property
     def files(self):
         '''
         Returns an iterable with all files from playlist, in order. This includes
         segments and key uri, if present.
         '''
-        return ()
+        raise NotImplementedError
 
     @property
     def playlists(self):
@@ -201,5 +181,96 @@ class M3U8(object):
 
         return playlists
 
+
+class BasePathMixin(object):
+
+    @property
+    def basepath(self):
+        return os.path.dirname(self.uri)
+
+    @basepath.setter
+    def basepath(self, newbasepath):
+        self.uri = self.uri.replace(self.basepath, newbasepath)
+
+
+class Segment(BasePathMixin):
+    '''
+    A video segment from a M3U8 playlist
+
+    `uri`
+      a string with the segment uri
+
+    `title`
+      title attribute from EXTINF parameter
+
+    `duration`
+      duration attribute from EXTINF paramter
+
+    '''
+
+    def __init__(self, uri, duration=None, title=None):
+        self.uri = uri
+        self.duration = duration
+        self.title = title
+
+    def __str__(self):
+        output = ['#EXTINF:%s,' % self.duration]
+        if self.title:
+            output.append(quoted(self.title))
+
+        output.append('\n')
+        output.append(self.uri)
+
+        return ''.join(output)
+
+
+class SegmentList(list):
+
+    def __str__(self):
+        output = [str(segment) for segment in self]
+        return '\n'.join(output)
+
+    def _set_basepath(self, newbasepath):
+        for segment in self:
+            segment.basepath = newbasepath
+
+    basepath = property(None, _set_basepath)
+
+
+class Key(BasePathMixin):
+    '''
+    Key used to encrypt the segments in a m3u8 playlist (EXT-X-KEY)
+
+    `method`
+      is a string. ex.: "AES-128"
+
+    `uri`
+      is a string. ex:: "https://priv.example.com/key.php?r=52"
+
+    `iv`
+      initialization vector. a string representing a hexadecimal number. ex.: 0X12A
+
+    '''
+    def __init__(self, method, uri, iv=None):
+        self.method = method
+        self.uri = uri
+        self.iv = iv
+
+    def __str__(self):
+        output = [
+            'METHOD=%s' % self.method,
+            'URI="%s"' % self.uri,
+            ]
+        if self.iv:
+            output.append('IV=%s' % self.iv)
+
+        return '#EXT-X-KEY:' + ','.join(output)
+
+
+
+
 def denormalize_attribute(attribute):
     return attribute.replace('_','-').upper()
+
+def quoted(string):
+    return '"%s"' % string
--- tests/test_model.py
+++ tests/test_model.py
@@ -9,21 +9,22 @@ from playlists import *
 
 def test_target_duration_attribute():
     obj = m3u8.M3U8(SIMPLE_PLAYLIST)
-    obj.data = {'targetduration': '1234567'}
+    mock_parser_data(obj, {'targetduration': '1234567'})
 
     assert '1234567' == obj.target_duration
 
 def test_media_sequence_attribute():
     obj = m3u8.M3U8(SIMPLE_PLAYLIST)
-    obj.data = {'media_sequence': '1234567'}
+    mock_parser_data(obj, {'media_sequence': '1234567'})
 
     assert '1234567' == obj.media_sequence
 
 def test_key_attribute():
     obj = m3u8.M3U8(SIMPLE_PLAYLIST)
-    obj.data = {'key': {'method': 'AES-128',
-                        'uri': '/key',
-                        'iv': 'foobar'}}
+    data = {'key': {'method': 'AES-128',
+                    'uri': '/key',
+                    'iv': 'foobar'}}
+    mock_parser_data(obj, data)
 
     assert 'Key' == obj.key.__class__.__name__
     assert 'AES-128' == obj.key.method
@@ -32,14 +33,14 @@ def test_key_attribute():
 
 def test_key_attribute_on_none():
     obj = m3u8.M3U8(SIMPLE_PLAYLIST)
-    obj.data = {}
+    mock_parser_data(obj, {})
 
     assert None == obj.key
 
 def test_key_attribute_without_initialization_vector():
     obj = m3u8.M3U8(SIMPLE_PLAYLIST)
-    obj.data = {'key': {'method': 'AES-128',
-                        'uri': '/key'}}
+    mock_parser_data(obj, {'key': {'method': 'AES-128',
+                                   'uri': '/key'}})
 
     assert 'AES-128' == obj.key.method
     assert '/key' == obj.key.uri
@@ -47,63 +48,60 @@ def test_key_attribute_without_initialization_vector():
 
 def test_segments_attribute():
     obj = m3u8.M3U8(SIMPLE_PLAYLIST)
-    obj.data = {'segments': [{'uri': '/foo/bar-1.ts',
-                              'title': 'First Segment',
-                              'duration': 1500},
-                             {'uri': '/foo/bar-2.ts',
-                              'title': 'Second Segment',
-                              'duration': 1600}]}
+    mock_parser_data(obj, {'segments': [{'uri': '/foo/bar-1.ts',
+                                         'title': 'First Segment',
+                                         'duration': 1500},
+                                        {'uri': '/foo/bar-2.ts',
+                                         'title': 'Second Segment',
+                                         'duration': 1600}]})
 
     assert 2 == len(obj.segments)
 
-    assert 'Segment' == obj.segments[0].__class__.__name__
     assert '/foo/bar-1.ts' == obj.segments[0].uri
     assert 'First Segment' == obj.segments[0].title
     assert 1500 == obj.segments[0].duration
 
 def test_segments_attribute_without_title():
     obj = m3u8.M3U8(SIMPLE_PLAYLIST)
-    obj.data = {'segments': [{'uri': '/foo/bar-1.ts',
-                              'duration': 1500}]}
+    mock_parser_data(obj, {'segments': [{'uri': '/foo/bar-1.ts',
+                                         'duration': 1500}]})
 
     assert 1 == len(obj.segments)
 
-    assert 'Segment' == obj.segments[0].__class__.__name__
     assert '/foo/bar-1.ts' == obj.segments[0].uri
     assert 1500 == obj.segments[0].duration
     assert None == obj.segments[0].title
 
 def test_segments_attribute_without_duration():
     obj = m3u8.M3U8(SIMPLE_PLAYLIST)
-    obj.data = {'segments': [{'uri': '/foo/bar-1.ts',
-                              'title': 'Segment title'}]}
-
+    mock_parser_data(obj, {'segments': [{'uri': '/foo/bar-1.ts',
+                                         'title': 'Segment title'}]})
 
     assert 1 == len(obj.segments)
 
-    assert 'Segment' == obj.segments[0].__class__.__name__
     assert '/foo/bar-1.ts' == obj.segments[0].uri
     assert 'Segment title' == obj.segments[0].title
     assert None == obj.segments[0].duration
 
 def test_is_variant_attribute():
     obj = m3u8.M3U8(SIMPLE_PLAYLIST)
-    obj.data = {'is_variant': False}
+    mock_parser_data(obj, {'is_variant': False})
     assert not obj.is_variant
 
-    obj.data = {'is_variant': True}
+    mock_parser_data(obj, {'is_variant': True})
     assert obj.is_variant
 
 def test_playlists_attribute():
     obj = m3u8.M3U8(SIMPLE_PLAYLIST)
-    obj.data = {'playlists': [{'resource': '/url/1.m3u8',
-                               'stream_info': {'program_id': '1',
-                                               'bandwidth': '320000'}},
-                               {'resource': '/url/2.m3u8',
-                                'stream_info': {'program_id': '1',
-                                                'bandwidth': '120000',
-                                                'codecs': 'mp4a.40.5'}},
-                                ]}
+    data = {'playlists': [{'resource': '/url/1.m3u8',
+                           'stream_info': {'program_id': '1',
+                                           'bandwidth': '320000'}},
+                          {'resource': '/url/2.m3u8',
+                           'stream_info': {'program_id': '1',
+                                           'bandwidth': '120000',
+                                           'codecs': 'mp4a.40.5'}},
+                          ]}
+    mock_parser_data(obj, data)
 
     assert 2 == len(obj.playlists)
 
@@ -119,9 +117,9 @@ def test_playlists_attribute():
 
 def test_playlists_attribute_without_program_id():
     obj = m3u8.M3U8(SIMPLE_PLAYLIST)
-    obj.data = {'playlists': [{'resource': '/url/1.m3u8',
-                               'stream_info': {'bandwidth': '320000'}}
-                              ]}
+    mock_parser_data(obj, {'playlists': [{'resource': '/url/1.m3u8',
+                                          'stream_info': {'bandwidth': '320000'}}
+                                         ]})
 
     assert 1 == len(obj.playlists)
 
@@ -132,19 +130,18 @@ def test_playlists_attribute_without_program_id():
 
 def test_version_attribute():
     obj = m3u8.M3U8(SIMPLE_PLAYLIST)
-    obj.data = {'version': '2'}
+    mock_parser_data(obj, {'version': '2'})
     assert '2' == obj.version
 
-
-    obj.data = {}
+    mock_parser_data(obj, {})
     assert None == obj.version
 
 def test_allow_cache_attribute():
     obj = m3u8.M3U8(SIMPLE_PLAYLIST)
-    obj.data = {'allow_cache': 'no'}
+    mock_parser_data(obj, {'allow_cache': 'no'})
     assert 'no' == obj.allow_cache
 
-    obj.data = {}
+    mock_parser_data(obj, {})
     assert None == obj.allow_cache
 
 # dump m3u8
@@ -175,8 +172,51 @@ def test_dump_should_create_sub_directories(tmpdir):
 
     assert_file_content(filename, expected)
 
+def test_if_basepath_is_passed_segments_and_key_urls_are_normalized():
+    basepath = 'http://videoserver.com/hls/live'
+
+    obj = m3u8.M3U8(PLAYLIST_WITH_ENCRIPTED_SEGMENTS_AND_IV, basepath)
+
+    expected = PLAYLIST_WITH_ENCRIPTED_SEGMENTS_AND_IV \
+        .replace(', IV', ',IV') \
+        .replace('../../../../hls', basepath) \
+        .replace('/hls-key', basepath) \
+        .strip()
+
+    result = obj.dumps()
+
+    assert result == expected
+
+def test_if_basepath_is_updated_segments_and_key_urls_are_normalized():
+    basepath = 'http://videoserver.com/hls/live'
+
+    obj = m3u8.M3U8(PLAYLIST_WITH_ENCRIPTED_SEGMENTS_AND_IV)
+    obj.basepath = basepath     # update later
+
+    expected = PLAYLIST_WITH_ENCRIPTED_SEGMENTS_AND_IV \
+        .replace(', IV', ',IV') \
+        .replace('../../../../hls', basepath) \
+        .replace('/hls-key', basepath) \
+        .strip()
+
+    result = obj.dumps()
+
+    assert result == expected
+
+
+
+# custom asserts
+
 def assert_file_content(filename, expected):
     with open(filename) as fileobj:
         content = fileobj.read().strip()
 
     assert content == expected
+
+
+# helpers
+
+def mock_parser_data(m3u8_obj, data):
+    data.setdefault('segments', [])
+    m3u8_obj.data = data
+    m3u8_obj._initialize_attributes()
