ed4afadecab9486cfba0db5b9b1ff343a629bc8c
Juarez Bochi <jbochi@gmail.com>
2012-09-27 14:48:47 -0300

    Bump up to 0.1.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="m3u8",
     author='Globo.com',
-    version="0.1.1",
+    version="0.1.2",
     zip_safe=False,
     packages=["m3u8"],
     url="https://github.com/globocom/m3u8",
