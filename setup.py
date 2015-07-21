3811ec4fc69ab1fefd0562f843b2645cacb59733
Mihai Mocanu <mvmocanu@users.noreply.github.com>
2015-07-21 15:57:31 +0300

    Bumped version to 0.2.6

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     name="m3u8",
     author='Globo.com',
     author_email='videos3@corp.globo.com',
-    version="0.2.5",
+    version="0.2.6",
     zip_safe=False,
     include_package_data=True,
     install_requires=install_reqs,
