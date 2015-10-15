6893fa1ae28168b04904109ec2b277372dd3811d
Leandro Moreira <leandro.ribeiro.moreira@gmail.com>
2015-10-15 11:09:30 -0300

    fixes cue_out failing test

--- tests/test_model.py
+++ tests/test_model.py
@@ -60,11 +60,10 @@ def test_segment_discontinuity_attribute():
 def test_segment_cue_out_attribute():
     obj = m3u8.M3U8(playlists.CUE_OUT_PLAYLIST)
     segments = obj.segments
-    print segments[0].__dict__
 
     assert segments[1].cue_out == True
     assert segments[2].cue_out == True
-    assert segments[3].cue_out == True
+    assert segments[3].cue_out == False
 
 def test_key_attribute():
     obj = m3u8.M3U8(playlists.SIMPLE_PLAYLIST)
