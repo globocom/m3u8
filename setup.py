99510ec00645a9b7687a8436891b35151184c14b
Leandro Moreira <leandro.ribeiro.moreira@gmail.com>
2016-07-03 22:03:08 -0300

    bumps up version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     name="m3u8",
     author='Globo.com',
     author_email='videos3@corp.globo.com',
-    version="0.2.8",
+    version="0.2.9",
     license='MIT',
     zip_safe=False,
     include_package_data=True,
