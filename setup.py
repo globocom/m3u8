4fdec99aaf735ebb41c2913d4ef1cd69b80d5267
Leandro Moreira <leandro.ribeiro.moreira@gmail.com>
2016-08-08 18:20:54 -0300

    releases new version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     name="m3u8",
     author='Globo.com',
     author_email='videos3@corp.globo.com',
-    version="0.2.9",
+    version="0.2.10",
     license='MIT',
     zip_safe=False,
     include_package_data=True,
