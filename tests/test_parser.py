c20ff50f5d033dd5e1b8c151b937e25ef894c3c6
Colin Dunklau <colin.dunklau@gmail.com>
2018-10-11 16:59:45 +0200

    Dequote CLOSED-CAPTIONS AttributeValue in STREAM-INF

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -115,6 +115,28 @@ def test_should_parse_variant_playlist():
     assert 65000 == playlists_list[-1]['stream_info']['bandwidth']
     assert 'mp4a.40.5,avc1.42801e' == playlists_list[-1]['stream_info']['codecs']
 
+def test_should_parse_variant_playlist_with_cc_subtitles_and_audio():
+    data = m3u8.parse(playlists.VARIANT_PLAYLIST_WITH_CC_SUBS_AND_AUDIO)
+    playlists_list = list(data['playlists'])
+
+    assert True == data['is_variant']
+    assert None == data['media_sequence']
+    assert 2 == len(playlists_list)
+
+    assert 'http://example.com/with-cc-hi.m3u8' == playlists_list[0]['uri']
+    assert 1 == playlists_list[0]['stream_info']['program_id']
+    assert 7680000 == playlists_list[0]['stream_info']['bandwidth']
+    assert 'cc' == playlists_list[0]['stream_info']['closed_captions']
+    assert 'sub' == playlists_list[0]['stream_info']['subtitles']
+    assert 'aud' == playlists_list[0]['stream_info']['audio']
+
+    assert 'http://example.com/with-cc-low.m3u8' == playlists_list[-1]['uri']
+    assert 1 == playlists_list[-1]['stream_info']['program_id']
+    assert 65000 == playlists_list[-1]['stream_info']['bandwidth']
+    assert 'cc' == playlists_list[-1]['stream_info']['closed_captions']
+    assert 'sub' == playlists_list[-1]['stream_info']['subtitles']
+    assert 'aud' == playlists_list[-1]['stream_info']['audio']
+
 def test_should_parse_variant_playlist_with_average_bandwidth():
     data = m3u8.parse(playlists.VARIANT_PLAYLIST_WITH_AVERAGE_BANDWIDTH)
     playlists_list = list(data['playlists'])
