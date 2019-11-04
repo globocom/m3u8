e3387f59a9c332b6f0593b49e82587089458bc59
Maurício Antunes <mauricio.abreua@gmail.com>
2019-11-04 08:06:07 -0300

    Bump version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     name="m3u8",
     author='Globo.com',
     author_email='videos3@corp.globo.com',
-    version="0.5.0",
+    version="0.5.1",
     license='MIT',
     zip_safe=False,
     include_package_data=True,
