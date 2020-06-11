93002afabde832172e07b26c01c0c3434274a1eb
Maurício Antunes <mauricio.abreua@gmail.com>
2020-06-11 09:11:25 -0300

    Allow users to use their own HTTP clients

--- m3u8/__init__.py
+++ m3u8/__init__.py
@@ -4,9 +4,7 @@
 # license that can be found in the LICENSE file.
 
 import sys
-import ssl
 import os
-import posixpath
 
 try:
     from urllib.request import urlopen, Request
@@ -16,6 +14,7 @@ except ImportError:  # Python 2.x
     from urllib2 import urlopen, Request, HTTPError
     from urlparse import urlparse, urljoin
 
+from m3u8.httpclient import DefaultHTTPClient, _parsed_url
 from m3u8.model import (M3U8, Segment, SegmentList, PartialSegment,
                         PartialSegmentList, Key, Playlist, IFramePlaylist,
                         Media, MediaList, PlaylistList, Start,
@@ -24,7 +23,6 @@ from m3u8.model import (M3U8, Segment, SegmentList, PartialSegment,
                         DateRangeList)
 from m3u8.parser import parse, is_url, ParseError
 
-PYTHON_MAJOR_VERSION = sys.version_info
 
 __all__ = ('M3U8', 'Segment', 'SegmentList', 'PartialSegment',
             'PartialSegmentList', 'Key', 'Playlist', 'IFramePlaylist',
@@ -47,37 +45,16 @@ def loads(content, uri=None, custom_tags_parser=None):
         return M3U8(content, base_uri=base_uri, custom_tags_parser=custom_tags_parser)
 
 
-def load(uri, timeout=None, headers={}, custom_tags_parser=None, verify_ssl=True):
+def load(uri, timeout=None, headers={}, custom_tags_parser=None, http_client=DefaultHTTPClient(), verify_ssl=True):
     '''
     Retrieves the content from a given URI and returns a M3U8 object.
     Raises ValueError if invalid content or IOError if request fails.
     '''
     if is_url(uri):
-        return _load_from_uri(uri, timeout, headers, custom_tags_parser, verify_ssl)
+        content, base_uri = http_client.download(uri, timeout, headers, verify_ssl)
+        return M3U8(content, base_uri=base_uri, custom_tags_parser=custom_tags_parser)
     else:
         return _load_from_file(uri, custom_tags_parser)
-
-# Support for python3 inspired by https://github.com/szemtiv/m3u8/
-
-
-def _load_from_uri(uri, timeout=None, headers={}, custom_tags_parser=None, verify_ssl=True):
-    request = Request(uri, headers=headers)
-    context = None
-    if not verify_ssl:
-        context = ssl._create_unverified_context()
-    resource = urlopen(request, timeout=timeout, context=context)
-    base_uri = _parsed_url(resource.geturl())
-    content = resource.read().decode(
-        resource.headers.get_content_charset(failobj="utf-8")
-    )
-    return M3U8(content, base_uri=base_uri, custom_tags_parser=custom_tags_parser)
-
-
-def _parsed_url(url):
-    parsed_url = urlparse(url)
-    prefix = parsed_url.scheme + '://' + parsed_url.netloc
-    base_path = posixpath.normpath(parsed_url.path + '/..')
-    return urljoin(prefix, base_path)
 
 
 def _load_from_file(uri, custom_tags_parser=None):
---
+++ m3u8/httpclient.py
@@ -0,0 +1,58 @@
+import posixpath
+import ssl
+import sys
+
+try:
+    import urllib
+    from urllib.error import HTTPError
+    from urllib.parse import urlparse, urljoin
+except ImportError:  # Python 2.x
+    from urllib2 import urlopen, Request, HTTPError
+    from urlparse import urlparse, urljoin
+
+
+PYTHON_MAJOR_VERSION = sys.version_info
+
+
+def _parsed_url(url):
+    parsed_url = urlparse(url)
+    prefix = parsed_url.scheme + '://' + parsed_url.netloc
+    base_path = posixpath.normpath(parsed_url.path + '/..')
+    return urljoin(prefix, base_path)
+
+def _read_python2x(resource):
+    return resource.read().strip()
+
+
+def _read_python3x(resource):
+    return resource.read().decode(
+        resource.headers.get_content_charset(failobj="utf-8")
+    )
+
+
+class DefaultHTTPClient:
+
+    def __init__(self, proxies=None):
+        self.proxies = proxies
+
+    def download(self, uri, timeout=None, headers={}, verify_ssl=True):
+        https_handler = HTTPSHandler(verify_ssl=verify_ssl)
+        opener = urllib.request.build_opener(https_handler)
+        opener.addheaders = headers.items()
+        resource = opener.open(uri, timeout=timeout)
+        base_uri = _parsed_url(resource.geturl())
+        if PYTHON_MAJOR_VERSION < (3,):
+            content = _read_python2x(resource)
+        else:
+            content = _read_python3x(resource)
+        return content, base_uri
+
+
+class HTTPSHandler:
+
+    def __new__(self, verify_ssl=True):
+        context = ssl.create_default_context()
+        if not verify_ssl:
+            context.check_hostname = False
+            context.verify_mode = ssl.CERT_NONE
+        return urllib.request.HTTPSHandler(context=context)
