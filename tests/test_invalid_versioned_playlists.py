c3018c25e390570d12db23e10656e66e5462e16f
Maurício Antunes <mauricio.abreua@gmail.com>
2024-05-12 22:53:32 -0300

    test: Improve assertions

--- tests/test_invalid_versioned_playlists.py
+++ tests/test_invalid_versioned_playlists.py
@@ -9,16 +9,22 @@ import pytest
 import m3u8
 
 
-@pytest.mark.xfail
 def test_should_fail_if_iv_in_EXT_X_KEY_and_version_less_than_2():
-    m3u8.parse(invalid_versioned_playlists.M3U8_RULE_IV)
+    with pytest.raises(Exception) as exc_info:
+        m3u8.parse(invalid_versioned_playlists.M3U8_RULE_IV, strict=True)
+
+    assert "Change the protocol version to 2 or higher." in str(exc_info.value)
 
 
-@pytest.mark.xfail
 def test_should_fail_if_floating_point_EXTINF_and_version_less_than_3():
-    m3u8.parse(invalid_versioned_playlists.M3U8_RULE_FLOATING_POINT)
+    with pytest.raises(Exception) as exc_info:
+        m3u8.parse(invalid_versioned_playlists.M3U8_RULE_FLOATING_POINT, strict=True)
+
+    assert "Change the protocol version to 3 or higher." in str(exc_info.value)
 
 
-@pytest.mark.xfail
 def test_should_fail_if_EXT_X_BYTERANGE_or_EXT_X_I_FRAMES_ONLY_and_version_less_than_4():
-    m3u8.parse(invalid_versioned_playlists.M3U8_RULE_BYTE_RANGE)
+    with pytest.raises(Exception) as exc_info:
+        m3u8.parse(invalid_versioned_playlists.M3U8_RULE_BYTE_RANGE, strict=True)
+
+    assert "Change the protocol version to 4 or higher." in str(exc_info.value)
